0369a0deddd7bb47abbc0b764dcb8681d3b6422e
Шторк Андрей Олегович <shtork@lanit.ru>
2023-10-26 22:25:51 +0700

    настройки

--- backend/src/bot-chat/chat-actions.service.ts
+++ backend/src/bot-chat/chat-actions.service.ts
@@ -55,7 +55,7 @@ export class ChatActionsService {
     this.bot = bot;
 
     cron.schedule(
-      '0 30 18 * * *',
+      '0 30 20 * * *',
       async () => {
         const activeUsers = await this.userService.getAllActiveUsers();
         const blockedUsers: string[] = [];
